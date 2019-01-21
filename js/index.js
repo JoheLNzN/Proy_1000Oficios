786c7923463778a80375d5b31dec80f818f04de4
Joel C. Naupa Crispín <jnaupac@gmail.com>
2019-01-21 17:48:31 -0500

    Maquetación 1000 Oficios

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('input#txtEmail').focus();
+
+$('div.form-container div.c-input input.input').on('focus', function(){
+	$(this).parent().addClass('current-row-form');
+}).on('focusout', function(){
+	$(this).parent().removeClass('current-row-form');
+});
+
+$('button#btnLogin').on('click', function(){
+	window.location.href= "../main.html";
+});
