a14dac102d1f5dbfc0540b9b7841e96063abc636
Joel C. Naupa Crispín <jnaupac@gmail.com>
2019-01-27 04:54:20 -0500

    Create Account - Terminado

--- js/register.js
+++ js/register.js
@@ -1,1 +1,13 @@
 $('input#txtEmail').focus();
+
+$('button.btnCancel').on('click', function(e){ 
+	e.preventDefault();
+	window.location.href="../index.html"; 
+});
+
+
+$('button.btnOk').on('click', function(e){ 
+	e.preventDefault();
+	
+	alert('Validando campos ....ESTO FALTA :v');
+});
