a662b7331ad60b22432796035665cddc04f049c5
Joel C. Naupa Crispín <jnaupac@gmail.com>
2019-01-27 08:23:52 -0500

    Suscripción - Terminado

--- js/register.js
+++ js/register.js
@@ -8,6 +8,24 @@ $('button.btnCancel').on('click', function(e){
 
 $('button.btnOk').on('click', function(e){ 
 	e.preventDefault();
-	
-	alert('Validando campos ....ESTO FALTA :v');
+
+	/* Obtener el plan seleccionado*/
+
+	var $plan = $('ul.list-plans li.selected');
+
+	var $msg = $plan.length == 0 ? 'No ha seleccionado un plan.' : 
+	          'El plan seleccionado es de ' + ($plan.find('span.type-plan').html());
+
+	alert('Validando campos, ' + $msg);
+});
+
+/* Seleccionar un plan */
+
+$('ul.list-plans li').on('click', function(e){
+	e.preventDefault();
+
+	var $current = $(this);
+
+	$current.toggleClass('selected').siblings().removeClass('selected');
+
 });
