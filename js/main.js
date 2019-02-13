51dfb256417eab91546fd77041d60bfb6df3ac6b
Joel C. Naupa Crispín <jnaupac@gmail.com>
2019-02-13 09:46:55 -0500

    Cambios - Main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $('a#btnPulishService').on("click", function (e) {
     e.preventDefault();
-
+    
     var $modal = $('div#c-modal-create-service');
     $modal.fadeIn('slow');
 
-
     $modal.find('div.form-container').addClass('show');
-
 });
 
 $('button.btnCancel').on("click", function (e) {
